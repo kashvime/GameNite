f02f233dca0f9cb8fd2e7116e911118e375f196f
Ha Nguyen <nganha.nguyen3011@gmail.com>
2026-04-10 11:36:52 -0400

    fix network issues

--- client/src/hooks/useGameList.ts
+++ client/src/hooks/useGameList.ts
@@ -2,22 +2,19 @@ import type { ErrorMsg, GameInfo } from "@gamenite/shared";
 import { useEffect, useState } from "react";
 import { gameList } from "../services/gameService.ts";
 
-/**
- * Custom hook to get the list of all thread summaries and decide on an
- * appropriate message if that list is not available.
- * @param maxSummaries - the maximum number of summaries desired (default is all of them)
- * @returns A message to display to the user (Loading... or an error message), or a list
- */
+const PAGE_SIZE = 10;
+
 export default function useGameList(maxGames?: number): { message: string } | GameInfo[] {
   const [games, setGames] = useState<GameInfo[] | ErrorMsg | null>(null);
 
   useEffect(() => {
-    gameList().then(setGames);
-  }, []);
+    gameList(maxGames).then(setGames);
+  }, [maxGames]);
 
   if (!games) return { message: "Loading..." };
   if ("error" in games) return { message: `Error: ${games.error}` };
   if (games.length === 0) return { message: "No games found..." };
-  if (maxGames) return games.slice(0, maxGames);
   return games;
 }
+
+export { PAGE_SIZE };
